1992ecb9d78d2d35150e1cdec42929e75fb662e1
craftluis12 <88074367+craftluis12@users.noreply.github.com>
2025-02-03 11:34:19 -0500

    Add files via upload

--- autoscript.sh
+++ autoscript.sh
@@ -106,8 +106,8 @@ elif [ "$option" == "pack" ]; then
     useradd -m -G wheel -s /bin/bash $user
     echo "Please add a password for the user!"
     passwd $user
-    sed -i '/^#%wheel ALL=(ALL:ALL) ALL/s/^#//' /etc/sudoers #Uncomments %wheel ALL=(ALL:ALL) ALL
-    exit
+    sudo sed -i '/^#%wheel ALL=(ALL:ALL) ALL/s/^#//' /etc/sudoers #Uncomments %wheel ALL=(ALL:ALL) ALL
+    sudo sed -i '/^#%wheel ALL=(ALL) ALL/s/^#//' /etc/sudoers #Uncomments %wheel ALL=(ALL) ALL
     exit 0
 
 elif [ "$option" == "pm" ]; then
